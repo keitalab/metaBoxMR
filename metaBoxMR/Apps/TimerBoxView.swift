255b92f7480fc17634327f1fc60bd7601176c936
ぴくせる <48606231+PMGWork@users.noreply.github.com>
2024-11-27 14:43:45 +0900

    Update TimerBoxView.swift

--- metaBoxMR/Apps/TimerBoxView.swift
+++ metaBoxMR/Apps/TimerBoxView.swift
@@ -12,20 +12,19 @@ struct TimerBoxView: View {
     @State private var remainingTime: TimeInterval = 0
     @State private var selectedMinutes = 0
     @State private var selectedSeconds = 0
-    @State private var timerActive = false
-    @State private var isLocked = false
     
     let minutesRange = Array(0...59)
     let secondsRange = Array(0...59)
+    
+    @State private var currentView: Int = 0
     
     // タイマーを開始
     @MainActor
     func startTimer() {
         remainingTime = TimeInterval(selectedMinutes * 60 + selectedSeconds)
-        isLocked = true
         
         if remainingTime > 0 {
-            timerActive = true
+            currentView = 1
             Task {
                 while remainingTime > 0 {
                     try await Task.sleep(nanoseconds: 1_000_000_000)
@@ -33,7 +32,7 @@ struct TimerBoxView: View {
                     remainingTime -= 1
                     
                     if remainingTime <= 0 {
-                        timerActive = false
+                        currentView = 2
                         stopTimer()
                         break
                     }
@@ -44,14 +43,8 @@ struct TimerBoxView: View {
     
     // タイマーを停止
     func stopTimer() {
-        unlock()
         timer?.invalidate()
         timer = nil
-    }
-    
-    func unlockBox() {
-        isLocked = false
-        unlock()
     }
     
     // 残り時間のフォーマットを変更
@@ -61,63 +54,88 @@ struct TimerBoxView: View {
         return String(format: "%02d:%02d", minutes, seconds)
     }
     
-    // メインビュー
+    // メイン
     var body: some View {
+        ZStack {
+            switch currentView {
+            case 1:
+                lockView
+                    .transition(.blurReplace)
+            case 2:
+                unlockView
+                    .transition(.blurReplace)
+            default:
+                mainView
+                    .transition(.blurReplace)
+            }
+        }
+        .background(Color.clear)
+        .animation(.default, value: currentView)
+    }
+    
+    // タイマー設定
+    var mainView: some View {
         VStack {
             Text("タイマーBox")
                 .padding()
                 .font(.title)
-            VStack {
-                if timerActive {
-                    Image(systemName: "lock")
-                        .font(.system(size: 72, weight: .semibold))
-                        .padding()
-                    Text(formatTime(remainingTime))
-                        .font(.system(size: 72, weight: .semibold))
-                        .padding(3)
-                        .contentTransition(.numericText())
-                    Text("時間までロックします")
-                        .font(.system(size: 16, weight: .semibold))
-                } else if !isLocked {
-                    HStack {
-                        Picker(selection: $selectedMinutes, label: Text("分")) {
-                            ForEach(minutesRange, id: \.self) { minute in
-                                Text("\(minute) 分").tag(minute)
-                            }
-                        }
-                        .pickerStyle(WheelPickerStyle())
-                        .frame(width: 100)
-                        
-                        Picker(selection: $selectedSeconds, label: Text("秒")) {
-                            ForEach(secondsRange, id: \.self) { second in
-                                Text("\(second) 秒").tag(second)
-                            }
-                        }
-                        .pickerStyle(WheelPickerStyle())
-                        .frame(width: 100)
+            
+            HStack {
+                Picker(selection: $selectedMinutes, label: Text("分")) {
+                    ForEach(minutesRange, id: \.self) { minute in
+                        Text("\(minute) 分").tag(minute)
                     }
-                    .padding()
-                    
-                    Button("開始") {
-                        startTimer()
-                    }
-                    .padding()
-                } else {
-                    Image(systemName: "lock.open")
-                        .font(.system(size: 72, weight: .semibold))
-                        .padding()
-                    Button("解錠") {
-                        unlockBox()
-                    }
-                    .padding()
-                    Text("ロックが解除されました")
-                        .font(.system(size: 16, weight: .semibold))
                 }
+                .pickerStyle(WheelPickerStyle())
+                .frame(width: 100)
+                
+                Picker(selection: $selectedSeconds, label: Text("秒")) {
+                    ForEach(secondsRange, id: \.self) { second in
+                        Text("\(second) 秒").tag(second)
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                .frame(width: 100)
             }
-            .frame(width: 400, height: 300)
+            .padding()
+            
+            Button("開始") {
+                startTimer()
+            }
+            .padding()
         }
-        .frame(width: 400, height: 400)
-        .background(Color.black.opacity(0))
+    }
+    
+    // タイマー動作時
+    var lockView: some View {
+        VStack {
+            Image(systemName: "lock")
+                .font(.system(size: 72, weight: .semibold))
+                .padding()
+            Text(formatTime(remainingTime))
+                .font(.system(size: 72, weight: .semibold))
+                .padding(3)
+                .contentTransition(.numericText())
+            Text("時間までロックします")
+                .font(.title3)
+        }
+    }
+    
+    // タイマー終了時
+    var unlockView: some View {
+        VStack {
+            Text("ロックを解除できます")
+                .padding()
+                .font(.title)
+            
+            Button("解錠") {
+                currentView = 0
+                
+                // metaBoxを解錠
+                unlock()
+            }
+            .padding()
+        }
     }
     
     struct TimerBoxView_Previews: PreviewProvider {
