ddc3f838ef8d183ad0435766a8c0e74702e8281e
ぴくせる <48606231+PMGWork@users.noreply.github.com>
2024-12-17 11:30:14 +0900

    others

--- metaBoxMR/Helpers/Unlock.swift
+++ metaBoxMR/Helpers/Unlock.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 func unlock() {
 //    let urlString: String = "http://172.20.10.4/open"
-    let urlString: String = "http://192.168.100.131/open"
+    let urlString: String = "http://192.168.100.38/open"
     guard let url = URL(string: urlString) else { return }
     
     var request = URLRequest(url: url)
@@ -26,4 +26,6 @@ func unlock() {
     }
     
     task.resume()
+    
+    print("Unlock")
 }
